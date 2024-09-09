ecde83fef9e68d3fa2318930f8e020f38a792aff
Boyan-Apostolov <58187770+Boyan-Apostolov@users.noreply.github.com>
2024-09-09 19:28:14 +0200

    Folder structure established

---
+++ RentRide_BE/src/main/java/nl/fontys/s3/rentride_be/RentRideBeApplication.java
@@ -0,0 +1,13 @@
+package nl.fontys.s3.rentride_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentRideBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentRideBeApplication.class, args);
+    }
+
+}
