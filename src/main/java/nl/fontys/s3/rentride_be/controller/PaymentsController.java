1de3f5f7e567f951b9b0af7d7952b1486a8121d6
Boyan-Apostolov <58187770+Boyan-Apostolov@users.noreply.github.com>
2024-09-30 21:00:11 +0200

    Moved payment session creation to use case and not directly in controlelr

--- src/main/java/nl/fontys/s3/rentride_be/controller/PaymentsController.java
+++ src/main/java/nl/fontys/s3/rentride_be/controller/PaymentsController.java
@@ -5,7 +5,9 @@ import com.stripe.model.checkout.Session;
 import com.stripe.param.checkout.SessionCreateParams;
 import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpSession;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import nl.fontys.s3.rentride_be.business.useCases.payment.CreatePaymentSessionUseCase;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,73 +16,48 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @RestController
 @RequestMapping("payments")
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class PaymentsController {
-
-    @Value("${API_KEY_STRIPE}")
-    private String stripeApiKey;
-
-    @PostConstruct
-    public void init() {
-        Stripe.apiKey = stripeApiKey;
-    }
+   private CreatePaymentSessionUseCase createPaymentSessionUseCase;
 
     @GetMapping("/success")
-    public ModelAndView success(@RequestParam("session_id") String sessionId) {
+    public ResponseEntity success(@RequestParam("session_id") String sessionId) {
         try {
             Session session = Session.retrieve(sessionId);
             String paymentStatus = session.getPaymentStatus();
 
-            if ("paid".equals(paymentStatus)) {
-                return new ModelAndView("redirect:/cars");
+            if (Objects.equals(paymentStatus, "paid")) {
+                //TODO: activate booking
+                return ResponseEntity.ok("Payment successful");
             } else {
-                return new ModelAndView("redirect:/cars");
+                //TODO: cancel booking
+                return ResponseEntity.badRequest().build();
             }
         } catch (Exception e) {
-            return new ModelAndView("redirect:/cars");
+            return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 
     @GetMapping("/cancel")
-    public ModelAndView cancel() {
-        // Todo: Delete booking
-        return new ModelAndView("redirect:/cars");
+    public ResponseEntity cancel() {
+        //TODO: cancel booking
+
+        return ResponseEntity.ok("Payment cancelled");
     }
 
-    @GetMapping("/create-checkout-session")
-    public ResponseEntity<String> createCheckoutSession() {
+    @GetMapping("/pay")
+    public ResponseEntity<String> createCheckoutSession(@RequestParam("bookingId") Long bookingId) {
+        //Todo: get booking id and get price
+        Long price = 12L;
+
         try {
-            SessionCreateParams.LineItem.PriceData.ProductData productData =
-                    SessionCreateParams.LineItem.PriceData.ProductData.builder()
-                            .setName("Booking - {carNumber} - {city1} - {city2}") // Product name
-                            .build();
+            String url = this.createPaymentSessionUseCase.createPaymentSession("Booking - {carNumber} - {city1} - {city2}", price);
 
-            SessionCreateParams.LineItem.PriceData priceData =
-                    SessionCreateParams.LineItem.PriceData.builder()
-                            .setCurrency("eur")
-                            .setProductData(productData)
-                            .setUnitAmount(500L) // Amount in cents
-                            .build();
-
-            SessionCreateParams.LineItem lineItem =
-                    SessionCreateParams.LineItem.builder()
-                            .setPriceData(priceData)
-                            .setQuantity(1L)
-                            .build();
-
-            SessionCreateParams params = SessionCreateParams.builder()
-                    .addPaymentMethodType(SessionCreateParams.PaymentMethodType.CARD)
-                    .addLineItem(lineItem)
-                    .setMode(SessionCreateParams.Mode.PAYMENT)
-                    .setSuccessUrl("http://localhost:8080/payments/success?session_id={CHECKOUT_SESSION_ID}")  // Pass session_id
-                    .setCancelUrl("http://localhost:8080/payments/cancel")
-                    .build();
-
-            Session session = Session.create(params);
-
-            return ResponseEntity.ok(session.getUrl());
+            return ResponseEntity.ok(url);
         } catch (Exception e) {
             return ResponseEntity.status(500).body(e.getMessage());
         }
