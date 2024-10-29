aa2bc00f6ff79622afce21fe4a14405b46987438
Boyan-Apostolov <58187770+Boyan-Apostolov@users.noreply.github.com>
2024-10-29 23:20:38 +0100

    Add seeded cars

--- src/main/java/nl/fontys/s3/rentride_be/DatabaseDataInitializer.java
+++ src/main/java/nl/fontys/s3/rentride_be/DatabaseDataInitializer.java
@@ -1,5 +1,6 @@
 package nl.fontys.s3.rentride_be;
 
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import nl.fontys.s3.rentride_be.business.use_cases.booking.UpdateBookingStatusUseCase;
 import nl.fontys.s3.rentride_be.domain.car.CarFeatureType;
@@ -28,15 +29,17 @@ public class DatabaseDataInitializer {
 
 
     @EventListener(ApplicationReadyEvent.class)
+    @Transactional
     public void initializeDatabase() {
         populateCities();
         populateCarFeatures();
         populateUsers();
+        populateCars();
 
         tryFixMissedBookings();
     }
 
-    private void tryFixMissedBookings(){
+    private void tryFixMissedBookings() {
         LocalDateTime now = LocalDateTime.now();
 
         // Find paid bookings with passed start time that should be ACTIVE
@@ -55,10 +58,10 @@ public class DatabaseDataInitializer {
     }
 
     private void populateCarFeatures() {
-        if(carFeatureRepository.count() == 0){
+        if (carFeatureRepository.count() == 0) {
             this.carFeatureRepository.save(CarFeatureEntity.builder()
-                            .featureType(CarFeatureType.Seats)
-                            .featureText("5")
+                    .featureType(CarFeatureType.Seats)
+                    .featureText("5")
                     .build());
 
             this.carFeatureRepository.save(CarFeatureEntity.builder()
@@ -83,8 +86,8 @@ public class DatabaseDataInitializer {
         }
     }
 
-    private void populateCities(){
-        if(this.cityRepository.count() == 0){
+    private void populateCities() {
+        if (this.cityRepository.count() == 0) {
             String street = "Some street";
             this.cityRepository.save(CityEntity.builder().name("Eindhoven").lat(51.4231).lon(5.4623).depoAdress(street).build());
             this.cityRepository.save(CityEntity.builder().name("Amsterdam").lat(52.3676).lon(4.9041).depoAdress(street).build());
@@ -93,14 +96,73 @@ public class DatabaseDataInitializer {
         }
     }
 
+    private void populateCars() {
+        String featureNotFoundText = "Feature not found";
+        if (this.carRepository.count() == 0) {
+            CarEntity car1 = CarEntity.builder()
+                    .make("Ford")
+                    .model("Fiesta")
+                    .registrationNumber("BT2142KX")
+                    .fuelConsumption(6.5)
+                    .city(this.cityRepository.findById(1L).orElse(null)) // Assuming Eindhoven
+                    .photosBase64(List.of("https://i.ibb.co/WP7L59F/IMG-20220401-152405.jpg"))
+                    .build();
+
+            List<CarFeatureEntity> features1 = List.of(
+                    carFeatureRepository.findById(1L).orElseThrow(() -> new RuntimeException(featureNotFoundText)),
+                    carFeatureRepository.findById(2L).orElseThrow(() -> new RuntimeException(featureNotFoundText)),
+                    carFeatureRepository.findById(3L).orElseThrow(() -> new RuntimeException(featureNotFoundText)),
+                    carFeatureRepository.findById(5L).orElseThrow(() -> new RuntimeException(featureNotFoundText))
+            );
+            car1.setFeatures(features1);
+
+            CarEntity car2 = CarEntity.builder()
+                    .make("Audi")
+                    .model("RS5")
+                    .registrationNumber("AB1234CD")
+                    .fuelConsumption(12.4)
+                    .city(this.cityRepository.findById(2L).orElse(null)) // Assuming Amsterdam
+                    .photosBase64(List.of("https://i.ibb.co/5ccy8Qt/H4s-IAAAAAAAAAFvzlo-G1t-Ii-BOTrayfuvp-Gh6-m1z-Jga-Gig-IGBg-ZGo-Dh-Tt-NOaz-I-2-Dh-CHs-CEtz-Ew-F-Sl-Mw.png"))
+                    .build();
+
+            List<CarFeatureEntity> features2 = List.of(
+                    carFeatureRepository.findById(1L).orElseThrow(() -> new RuntimeException(featureNotFoundText)), // Seats
+                    carFeatureRepository.findById(2L).orElseThrow(() -> new RuntimeException(featureNotFoundText)), // Doors
+                    carFeatureRepository.findById(4L).orElseThrow(() -> new RuntimeException(featureNotFoundText)), // Transmission - Manual
+                    carFeatureRepository.findById(5L).orElseThrow(() -> new RuntimeException(featureNotFoundText))  // AC
+            );
+            car2.setFeatures(features2);
+
+            CarEntity car4 = CarEntity.builder()
+                    .make("BMW")
+                    .model("M5")
+                    .registrationNumber("GH9012IJ")
+                    .fuelConsumption(17.0)
+                    .city(this.cityRepository.findById(4L).orElse(null)) // Assuming Utrecht
+                    .photosBase64(List.of("https://i.ibb.co/fr5q7JP/bmw-m-series-seo-overview-ms-04.jpg"))
+                    .build();
+
+            List<CarFeatureEntity> features4 = List.of(
+                    carFeatureRepository.findById(1L).orElseThrow(() -> new RuntimeException(featureNotFoundText)),
+                    carFeatureRepository.findById(2L).orElseThrow(() -> new RuntimeException(featureNotFoundText)),
+                    carFeatureRepository.findById(3L).orElseThrow(() -> new RuntimeException(featureNotFoundText)),
+                    carFeatureRepository.findById(4L).orElseThrow(() -> new RuntimeException(featureNotFoundText)),
+                    carFeatureRepository.findById(5L).orElseThrow(() -> new RuntimeException(featureNotFoundText))
+            );
+            car4.setFeatures(features4);
+
+            this.carRepository.saveAll(List.of(car1, car2, car4));
+        }
+    }
+
     private void populateUsers() {
-        if(this.userRepository.count() == 0){
+        if (this.userRepository.count() == 0) {
             this.userRepository.save(UserEntity
                     .builder()
-                            .name("Boyan Apostolov")
-                            .role(UserRole.Admin)
-                            .email("admin@admin.com")
-                            .password("12345678")
+                    .name("Boyan Apostolov")
+                    .role(UserRole.Admin)
+                    .email("admin@admin.com")
+                    .password("12345678")
                     .birthDate(LocalDate.of(2004, 3, 12))
                     .build());
         }
