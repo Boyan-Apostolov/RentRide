3d181def7dcd332d9448860e2f408b54e1981534
Boyan-Apostolov <58187770+Boyan-Apostolov@users.noreply.github.com>
2024-11-13 14:18:54 +0100

    Fixed not passing tests

--- src/test/java/nl/fontys/s3/rentride_be/controller/CarsControllerTest.java
+++ src/test/java/nl/fontys/s3/rentride_be/controller/CarsControllerTest.java
@@ -6,8 +6,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -24,7 +27,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
-@WebMvcTest(CarsController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@WithMockUser(username = "10@fontys.nl", roles = {"ADMIN"})
 class CarsControllerTest {
 
     @Autowired
